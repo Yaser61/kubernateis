debe513f9a12dfe97dba14f11b01111ee0c4bdb3
Yaser <100582913+Yaser61@users.noreply.github.com>
2024-01-07 16:33:28 +0300

    transfer protocol ile mail gönderme (nodemailer)

---
+++ smtp.js
@@ -0,0 +1,25 @@
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  auth: {
+    user: "kalkanyaser@gmail.com",
+    pass: "lsik zzkg phns jscy"
+  }
+});
+
+const mailOptions = {
+  from: "Kocandan",
+  to: "rabiayilmazqwe@gmail.com",
+  subject: "Node.js ile SMTP Mail Gönderme",
+  text: "Node.js ve Nodemailer kullanarak SMTP üzerinden mail gönderimi hakkında bilgi edindiniz."
+};
+
+transporter.sendMail(mailOptions, function(err, info) {
+  if(err) {
+    console.log(err);
+  } else {
+    console.log("E-posta gönderildi: " + info.response);
+  }
+});
